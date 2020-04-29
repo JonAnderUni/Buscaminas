f58320174a3833861bacab3f7f86f053859bcb88
alain <alainjg971@gmail.com>
2020-04-29 16:46:55 +0200

    arreglado fallo interfaz

--- src/interfaz/Iu_HighScores.java
+++ src/interfaz/Iu_HighScores.java
@@ -216,8 +216,8 @@ public class Iu_HighScores extends JFrame {
 
 	private JButton getBtnNewButton_2() {
 		if (btnNewButton_2 == null) {
-			btnNewButton_2.setBackground(Color.LIGHT_GRAY);
 			btnNewButton_2 = new JButton("Dif\u00EDcil");
+			btnNewButton_2.setBackground(Color.LIGHT_GRAY);
 			btnNewButton_2.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					cargarDatos(dificil);
